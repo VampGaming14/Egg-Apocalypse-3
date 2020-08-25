22d4c8031dee1b1b079c8fe94b06a0b93985641b
VampGaming14 <65399588+VampGaming14@users.noreply.github.com>
2020-08-25 13:40:07 +0530

    Add files via upload

---
+++ man.js
@@ -0,0 +1,33 @@
+class Man extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.Visiblity = 255;
+    this.image = loadImage("pics/man1.png");
+    var engine = {
+      friction:50,
+      restitution:2
+    }
+  }
+
+  display(){
+    console.log(this.body.speed); 
+    if(this.body.speed < 4){
+      super.display();
+     }
+     else{
+       World.remove(world, this.body);
+       push();
+       this.Visiblity = this.Visiblity - 5;
+       tint(255,this.Visiblity);
+       image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+       pop();
+     } 
+  }
+
+  score(){
+    if (this.Visiblity < 0 && this.Visiblity > -1005){
+      score++;
+    }
+   }
+
+};
